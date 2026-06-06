bdabdd887b2bc909b24ce3bf48d0abd1ba26c9ac
Mohd Sufyaanuddin Khan <khansufyaan315@gmail.com>
2026-06-06 10:42:38 +0530

    Assignment 1 Done

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,8 +1,279 @@
-a = 20
-b = 10
+// PROBLEM 1
+// let name = "Sufyaan";
+// let age = 21;
+// let student = true;
+// let fav_color = "blue";
+// let no_siblings = 2;
 
-sum = a + b
-console.log("The sum of a and b is: " + sum)
+// console.log("My name is - " + name);
+// console.log("My age is - " + age);
+// console.log("I am Student or not - " + student);
+// console.log("My Favourite Color is - " + fav_color);
+// console.log("Number of Siblings I Have - " + no_siblings);
 
-difference = a - b
-console.log("The difference of a and b is: " + difference)
+// PROBLEM 2
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof student);
+// console.log(typeof fav_color);
+// console.log(typeof no_siblings);
+
+// PROBLEM 3
+// let score;
+// let winner = null;
+
+// console.log(score);
+// console.log(typeof score);
+
+// console.log(winner);
+// console.log(typeof winner);
+
+// PROBLEM 4
+// let fristName = "Mohd Sufyaanuddin"
+// let lastName = "Khan"
+// let fullName = fristName + " " + lastName
+
+// console.log(fullName)
+
+// PROBLEM 5
+// let a = "5"
+// let b = 3
+// let result = a + b
+// console.log(result)
+// console.log(typeof result);
+
+// PROBLEM 6
+// let num1 = 45
+// let num2 = 12
+
+// let add = num1 + num2
+// let sub = num1 - num2
+// let mul = num1 * num2
+// let quo = num1 / num2
+// let rem = num1 % num2;
+
+// console.log("Addition - " + add)
+// console.log("Difference - " + sub)
+// console.log("Product - " + mul)
+// console.log("Quoitent - " + quo)
+// console.log("Remainder - " + rem)
+
+// PROBLEM 7
+// let celsius = 25
+// let farenheit = (celsius * 9 / 5) + 32
+
+// console.log("25°C is equal to" + " " + farenheit + "°F")
+
+// PROBLEM 8
+// let radius = 7
+
+// let circumference = 2 * Math.PI * radius
+// let area = Math.PI * radius ^ 2
+
+// console.log("Circumference of Circle - " + circumference.toFixed(2))
+// console.log("Area of Circle - " + area.toFixed(2))
+
+// PROBLEM 9
+// let no_books = 3
+// let price_book = 45
+
+// let total_books = no_books * price_book
+
+// let no_pen = 2
+// let price_pen = 15
+
+// let total_pens = no_pen * price_pen
+
+// let price_bag = 850
+
+// let sub_total = total_books + total_pens + price_bag
+
+// console.log(sub_total)
+
+// let discount = sub_total * 0.1
+
+// let new_total = sub_total - discount
+
+// console.log(new_total)
+
+// PROBLEM 10
+// let number = 17
+
+// if (number % 2 === 0)
+//     console.log("The number is even")
+// else
+//     console.log("The number is odd")
+
+// PROBLEM 11
+// let result1 = 10 + 5 * 2
+// let result2 = (10 + 5) * 2
+// let result3 = 20 / 4 + 3 * 2
+// let result4 = 2 ** 3 + 5
+// let result5 = (8 + 2) / 5 * 3;
+
+// console.log(result1)
+// console.log(result2)
+// console.log(result3)
+// console.log(result4)
+// console.log(result5)
+
+// PROBLEM 12
+// let result = (((10 + 20) * 5) / 3)
+
+// console.log(result)
+
+// PROBLEM 13
+// let expression = ((((15 + 5) * 3) - 10) / 4)
+
+// let finalResult = expression
+
+// console.log(finalResult)
+
+// PROBLEM 14
+// let bank_balance = 5000
+// console.log(bank_balance)
+
+// bank_balance += 15000
+// console.log(bank_balance)
+
+// rent = bank_balance -= 8000
+// console.log(rent)
+
+// bonus = bank_balance += 5000
+// console.log(bonus)
+
+// shopping = bank_balance -= 3000
+// console.log(shopping)
+
+// final_balance = shopping
+// console.log(final_balance)
+
+// PROBLEM 15
+// let invest_amt = 100000
+
+// //year1
+// invest_amt *= 1.10
+// console.log(invest_amt)
+
+// //year2
+// invest_amt *= 1.10
+// console.log(invest_amt)
+
+// //year3
+// invest_amt *= 1.10
+// console.log(invest_amt)
+
+// PROBLEM 16
+// let x = 10
+// let y = x++
+
+// console.log("x: " + x + " y: " + y)
+
+// let a = 10
+// let b = ++a
+// console.log("a: " + a + " b: " + b);
+
+// PROBLEM 17
+// let = countdown = 10;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// countdown = --countdown;
+// console.log(countdown);
+
+// PROBLEM 18
+// let num = 5
+// let newNum = num++
+// console.log(newNum, num);
+
+// newNum = ++num
+// console.log(newNum, num)
+
+// PROBLEM 19
+// let result1 = 0 / 0
+// console.log(result1)
+
+// let result2 = "hello" * 5
+// console.log(result2)
+
+// let result3 = NaN + 10
+// console.log(result3)
+
+// let result4 = Math.sqrt(-1)
+// console.log(result4)
+
+// console.log(typeof NaN)
+
+// PROBLEM 20
+// let value1 = 100
+// let value2 = "test" / 2
+// let value3 = 0 / 0
+// let value4 = "123"
+
+// console.log(value1)
+// console.log(value2)
+// console.log(value3)
+// console.log(value4)
+
+// PROBLEM 21
+// let no_subjects = 5
+
+// let math_marks = 85
+// let science_marks = 92
+// let english_marks = 78
+// let history_marks = 88
+// let computer_marks = 95
+
+// total_marks = math_marks + science_marks + english_marks + history_marks + computer_marks
+
+// totalaverage_marks = total_marks / 5
+
+// if (totalaverage_marks >= 80)
+//     console.log("Your Average Grade is: A")
+// else if (totalaverage_marks >= 60)
+//     console.log("Your Average Grade is: B")
+// else if (totalaverage_marks >= 40)
+//     console.log("Your Average Grade is: C")
+// else
+//     console.log("Your Average Grade is: F" )
+
+// PROBLEM 22
+// let current_year = 2026
+// let birth_year = 2004
+
+// age = current_year - birth_year
+
+// console.log("In " + current_year + ", you are " + age + " years old" )
+
+// PROBLEM 23
+let a = 10
+let b = 20
+
+a += 10
+console.log("Value of a is - " + a)
+
+b -= 10
+console.log("Value of b is - " + b)
