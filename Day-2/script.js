e3a4d6082938cb783d41a44631763041a7b8dcc5
Mohd Sufyaanuddin Khan <khansufyaan315@gmail.com>
2026-06-06 18:22:20 +0530

    Day 2 Assignment

--- Day-2/script.js
+++ Day-2/script.js
@@ -1,8 +1,237 @@
-a = 20
-b = 10
+// PROBLEM 1
+// firstName - Valid
+// 2nd Place - Invalid
+// user_email - Valid
+// total - pricw - Invalid
+// $amount - Valid
+// my variable - Invalid
+// _privateData - Valid
+// let - Valid
 
-sum = a + b
-console.log("The sum of a and b is: " + sum)
+// PROBLEM 2
+// let userName = "Alice"
+// let prizeFirst = 1000
+// let totalCost = 250
+// let userAge = 25
 
-difference = a - b
-console.log("The difference of a and b is: " + difference)
+// PROBLEM 3
+// let userEmail = "user@gmail.com"
+// let userLoggedIn = true
+// const maxAttemps = 3
+// let studentFinalGrade = 85
+// let $productPrice = 99.99
+
+// PROBLEM 4
+// let isRaining = false
+// let userIsAdult = true
+// let passwordMatch = false
+
+// PROBLEM 5
+// let x = 10
+// console.log(typeof x)
+
+// x = "Hello"
+// console.log(typeof x)
+
+// x = true
+// console.log(typeof x)
+
+// PROBLEM 6
+// let language = "JavaScript"
+
+// console.log(language[0])
+// console.log(language[4])
+// console.log(language[9])
+// console.log(language.length)
+
+// PROBLEM 7
+// let message1 = "This is a string message in double quotes";
+// let message2 = "This is a string message in single quote";
+// let message3 = "Hello!!";
+
+// console.log(message1)
+// console.log(message2)
+// console.log(message3)
+
+// PROBLEM 8
+// let firstName = "Sufyaan"
+// let lastName = "Khan"
+
+// full_Name = firstName + lastName
+// console.log(full_Name)
+
+// greeting = "Hello, " + full_Name
+// console.log(greeting)
+
+// let age = 25
+// info = firstName + " is 22 years old"
+// console.log(info)
+
+// PROBLEM 9
+// let name = "Sufyaan"
+// let score = 99
+
+// let message = `Student ${name} scored ${score} points`
+
+// console.log(message)
+
+// let product = "Laptop"
+// let price = 999
+// let quantity = 2
+// let total = price * quantity
+
+// let receipt = `Item: ${product} ,Quantity: ${quantity} ,Total:$ ${total}`
+
+// console.log(receipt)
+
+// PROBLEM 10
+
+// let cityName = "Paris"
+// let temperature = 25
+
+// let weather = `The temperature in ${cityName} is ${temperature} degrees`
+
+// console.log(weather)
+
+// let hourOfDay = 14
+// let userName = 'Bob'
+
+// let greeting = `Good Afternoon, ${userName}! It's ${hourOfDay}:00.`
+
+// console.log(greeting)
+
+// PROBLEM 11
+// let score
+// console.log(score)
+
+// let winner = ''
+// console.log(winner)
+
+// let user = { name: "Alice" }
+// console.log(user.age)
+
+// PROBLEM 12
+
+// console.log(10 > 5);
+// console.log(3 < 2);
+// console.log(5 >= 5);
+// console.log(8 <= 10);
+// console.log(7 != 7);
+// console.log(15 > 20);
+
+// PROBLEM 13
+// let age = 18;
+// console.log(age >= 18);
+
+// let temperature = 30
+// console.log(temperature > 25)
+
+// let score = 75
+// console.log(score < 60)
+
+// PROBLEM 14
+// console.log(5==5);
+// console.log(5===5);
+// console.log(5=='5');
+// console.log(5==='5');
+// console.log(true==1);
+// console.log(true===1);
+// console.log(0==false);
+// console.log(0 === false);
+
+// PROBLEM 15
+// let userInput = 10
+// if (userInput === 10) {
+//     console.log("Input is 10")
+// }
+
+// PROBLEM 16
+// console.log("a" > "A");
+// console.log("b" < "c");
+// console.log("apple" < "banana");
+// console.log("Z" < "a");
+// console.log("10" < "2");
+
+// PROBLEM 17
+// let str1 = "Hello"
+// let str2 = "HELLO"
+
+// console.log(str1 !== str2)
+
+// PROBLEM 18
+// let userName = "Sarah"
+// let hour = 9
+
+// let greeting = `Good Morning, ${userName}`
+
+// console.log(greeting)
+
+// PROBLEM 19
+// let userAge = 16
+// let minimumAge = 18
+
+// let isOldEnough = userAge >= minimumAge
+
+// console.log(isOldEnough)
+
+// PROBLEM 20
+// let email = "alice@example.com"
+
+// let firstChar = email[0]
+
+// let isValid = firstChar >= 'a' && firstChar <= 'z'
+
+// console.log(isValid)
+
+// PROBLEM 21
+// let productName = "Wireless Mouse"
+// let productPrice = 29.99
+// let inStock = true
+
+// let description = `Product: ${productName} | Price: ${productPrice} | In Stock: ${inStock}`
+
+// console.log(description)
+
+// PROBLEM 22
+// let firstName = "John"
+// let middleName = "Robert"
+// let lastName = "Smith"
+
+// let initials = firstName[0] + "." + middleName[0] + "." + lastName[0] + ".";
+
+// console.log(initials)
+
+// PROBLEM 23
+// let firstPlace = "Gold"
+// let userName = "Alice"
+// let total_cost = 100
+// let userAge = 18
+
+// if (userAge == 18) {
+//     console.log("You can Vote")
+// }
+
+// let message = "It's a beautiful Day"
+// console.log(message)
+
+// PROBLEM 24
+// let userInput = "25"
+
+// if (userInput == 25) {
+//     console.log("Input is correct")
+// } else {
+//     console.log("Input is incorrect")
+// }
+
+// PROBLEM 25
+// let password = "Pass123";
+
+// let isLongEnough = password.length >= 6;
+
+// let startsWithUppercase = password[0] >= "A" && password[0] <= "Z";
+
+// let endsWithNumber = password[password.length - 1] >= '0' && password[password.length - 1] <= '9';
+
+// console.log("Password is long enough: ", isLongEnough);
+// console.log("Starts with uppercase: ", startsWithUppercase);
+// console.log("Ends with number: ", endsWithNumber);
