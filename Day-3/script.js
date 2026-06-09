fa40209e5bcb77a96839e29af94a32f438536b37
Mohd Sufyaanuddin Khan <khansufyaan315@gmail.com>
2026-06-09 16:20:57 +0530

    Day 3 Assignment

--- Day-3/script.js
+++ Day-3/script.js
@@ -1,8 +1,245 @@
-a = 20
-b = 10
+// PROBLEM 1
+// let num = -5
 
-sum = a + b
-console.log("The sum of a and b is: " + sum)
 
-difference = a - b
-console.log("The difference of a and b is: " + difference)
+// if (num < 0) {
+//     console.log("This is a negative number")
+// } else {
+//     console.log("It is a postive number or ZERO")
+// }
+
+// PROBLEM 2
+// let age = 17
+
+// if (age >= 18) {
+//     console.log("This person is eligible for Driver's License")
+// } else {
+//     console.log("This person is not eleigble for Driver's License")
+// }
+
+// PROBLEM 3
+// let marks = 55
+
+// if (marks >= 90) {
+//     console.log("A Grade")
+// } else if (marks >= 80 && marks <= 89) {
+//     console.log("B Grade")
+// } else if (marks >= 70 && marks <= 79){
+//     console.log("C Grade")
+// } else if (marks >= 60 && marks <= 69) {
+//     console.log("D Grade")
+// } else {
+//     console.log("F Grade")
+// }
+
+// PROBLEM 4
+// let userName = "admin"
+// let userPassword = "12345"
+
+// if (userName === "admin" && userPassword === "12345") {
+//     console.log("Login Successfull")
+// } else {
+//     console.log("Login Failed")
+// }
+
+// PROBLEM 5
+// let day = "Monday"
+
+// if (day === "Saturday" || day === "Sunday") {
+//     console.log("Weekend")
+// } else {
+//     console.log("Weekday")
+// }
+
+// PROBLEM 6
+// let age = 25
+
+// if (age < 12) {
+//     console.log("Ticket Costs $5")
+// } else if (age >= 12 && age < 60) {
+//     console.log("Ticekt Costs $8")
+// } else if (age >= 60) {
+//     console.log("Ticket Costs $6")
+// } else {
+//     console.log("Wrong Age")
+// }
+
+// PROBLEM 7
+// let orderTotal = 40
+// let isPremium = true
+
+// if (orderTotal > 50 || isPremium === true) {
+//     console.log("Free Shipping")
+// } else {
+//     console.log("No Free Shipping")
+// }
+
+// PROBLEM 8
+// let marks = 90
+
+// if (marks >= 35) {
+//     if (marks >= 80) {
+//         console.log("Pass with distinction")
+//     } else {
+//         console.log("Pass")
+//     }
+// } if (marks < 33) {
+//     console.log("Fail")
+// }
+
+// PROBLEM 9
+// let temperature = 25
+// let isRaining = false
+
+// if (temperature > 30) {
+//     console.log("Hot Day")
+// } if (temperature >= 20 && temperature <= 30) {
+//     if (isRaining === true) {
+//         console.log("Pleasant but rainy")
+//     } else {
+//         console.log("Perfect weather")
+//     }
+// } if (temperature < 20) {
+//     console.log("Cold Day")
+// }
+
+// PROBLEM 10
+// let dayNum = 7
+
+// switch (dayNum) {
+//     case 1:
+//         console.log("Monday")
+//         break
+//     case 2:
+//         console.log("Tuesday")
+//         break
+//     case 3:
+//         console.log("Wednesday")
+//         break
+//     case 4:
+//         console.log("Thursday")
+//         break
+//     case 5:
+//         console.log("Friday")
+//         break
+//     case 6:
+//         console.log("Saturday")
+//         break
+//     case 7:
+//         console.log("Sunday")
+//         break
+//     default:
+//         console.log("Wrong Number Entered")
+// }
+
+// PROBLEM 11
+// let signal = "yellow"
+
+// switch (signal) {
+//     case "red":
+//         console.log("Stop")
+//         break
+//     case "yellow":
+//         console.log("Caution")
+//         break
+//     case "green":
+//         console.log("Go")
+//         break
+//     default:
+//         console.log("Invalid Signal")
+// }
+
+// PROBLEM 12
+// let month = 11
+
+// switch (month) {
+//     case 1:
+//         console.log("Winter")
+//         break
+//     case 2:
+//         console.log("Winter")
+//         break
+//     case 3:
+//         console.log("Spring")
+//         break
+//     case 4:
+//         console.log("Spring")
+//         break
+//     case 5:
+//         console.log("Spring")
+//         break
+//     case 6:
+//         console.log("Summer")
+//         break
+//     case 7:
+//         console.log("Summer")
+//         break
+//     case 8:
+//         console.log("Summer")
+//         break
+//     case 9:
+//         console.log("Fall")
+//         break
+//     case 10:
+//         console.log("Fall")
+//         break
+//     case 11:
+//         console.log("Fall")
+//         break
+//     case 12:
+//         console.log("Winter")
+//         break
+//     default:
+//         console.log("Wrong Number Entered")
+// }
+
+// PROBLEM 13
+// let userName = ""
+
+// if (userName) {
+//     console.log(userName)
+// } else {
+//     console.log("Please enter a usernam")
+// }
+
+// PROBLEM 14
+// let score = 0
+
+// if (score) {
+//     console.log(score)
+// } else {
+//     console.log("No Score Yet")
+// }
+
+// PROBLEM 15
+// let age = 20
+// let isCitizen = true
+// let hasVoted = false
+
+// if (age >= 18 && isCitizen && hasVoted) {
+//     console.log("You can Vote")
+// } else {
+//     console.log("You cannot vote")
+// }
+
+// PROBLEM 16
+// let total = 120
+// let isPremium = false
+
+// if (isPremium) {
+//     premiumDiscount = total * 0.2
+//     finalPrice3 = total - premiumDiscount
+//     console.log("After Discount Total is: ",finalPrice3 )
+// } else if (total > 100) {
+//     discount1 = total * 0.15
+//     finalPrice1 = total - discount1
+//     console.log("After 15% Discount Total is: ", finalPrice1)
+// } else if (total > 50) {
+//     discount2 = total * 0.1
+//     finalPrice2 = total - discount2
+//     console.log("After 10% Discount Total is: ", finalPrice2)
+// } else {
+//     console.log("No Discount")
+// }
+
+// PROBLEM 17
